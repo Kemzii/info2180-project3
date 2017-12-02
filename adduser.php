1e464a36158f4e2c72f6fbf0cf8616e1af425747
kemzii <kemar777@yahoo.com>
2017-12-02 20:08:12 +0000

    Touches

---
+++ adduser.php
@@ -0,0 +1,118 @@
+<?php
+if(isset($_POST["login"]))
+{
+    $host = getenv('IP');
+    $username = getenv('C9_USER');
+    $password = '';
+    $dbname = 'CheapoMail';
+    
+    try{
+        //Create Connection With The Database
+     
+        $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+       
+    	//echo "Connection Made";
+    	// set the PDO error mode to exception
+    
+
+        
+        $type=$_POST['Type'];
+        $username=$_POST['username'];
+        $password=$_POST['password'];
+        $hash = md5($password);
+        
+        
+        if($type == 'admin'){
+          
+      
+             $stmt = $conn->query("SELECT * FROM User WHERE id = 1 ");   
+             
+			
+			           $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+              foreach ($results as $row) {
+                   //echo "In results";
+                   if($row['username'] == $username){
+                      //echo "Username in database is:" . $row['username'] ;
+                      //echo "Username is:" .  $username ;
+                      $usernameok = true;
+                   }else {
+                      //echo "Username in database is:" . $row['username'] ;
+                     // echo "Username is:" .  $username ;
+                       $usernameok=false;
+                   }
+                   if($row['password'] == $hash){
+                      //echo "Password in database is:" .$row['password'] ;
+                      //echo "Password is:" .  $hash ;
+                       $passwordok = true;
+                   }
+                   else{
+                    //  echo "Password in database is:" .$row['password'] ;
+                     // echo "Password is:" .  $hash ;
+                       $passwordok = false;
+                   }
+                // echo $ok;  
+              }
+              if($usernameok == true && $passwordok ==true) { 
+                 //echo "Admin" ;
+                 include("adduserform.html"); 
+                  
+              }
+              else{
+               
+                  include("userlogin.html");
+              }
+              
+        }
+        else{
+              $stmt = $conn->query("SELECT * FROM User ");   
+             //echo "In else" . "\n";
+			          $userfound =false;
+			          $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+              foreach ($results as $row) {
+                  
+                 if($row['username'] == $username){
+                    // echo "Username in database is:" . $row['username'] . "\n";
+                    $ok = true;
+                   }else {
+                   // echo "Username in database is:" . $row['username']  . "\n" ;
+                    // echo "Username is:" .  $username . "\n" ;
+                       $ok=false;
+                   }
+                   if($row['password'] == $hash){
+                      //echo "Password in database is:" .$row['password']. "\n" ;
+                      //echo "Password is:" .  $hash . "\n";
+                       $ok = true;
+                   }
+                   else{
+                  //  echo "Password in database is:" .$row['password'] . "\n";
+                    // echo "Password is:" .  $hash. "\n" ;
+                       $ok = false;
+                   }
+                   
+                   if($ok){$userfound=true;}
+              }
+              
+              if($userfound) { 
+                        //echo "Everything is ok";
+                        session_start();
+
+                        $_SESSION["username"] = $username;
+                       
+                        include("home.html");
+              }
+              else {
+               
+                   include("userlogin.html");
+              }
+              
+        }
+        
+        
+       
+    }
+    catch(PDOException $e)
+	{	
+		echo $sql . "<br>" . $e->getMessage();;
+	}
+}
+?>
---
+++ schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS CheapoMail;
+CREATE DATABASE CheapoMail;
+USE CheapoMail;
+
+CREATE TABLE User
+(
+id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+firstname varchar(255),
+lastname varchar(255),
+username varchar(255),
+password varchar(255)
+);
+
+INSERT INTO User(id, firstname, lastname, username, password) VALUES
+(1, 'Some', 'One', 'Legendary', '6437db2022d0b8c92fe6e87731d894cc');
+
+CREATE TABLE Message (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+recipient_id INT UNSIGNED, 
+user_id INT UNSIGNED,
+subject varchar(255),
+body text,
+date_sent date
+);
+
+INSERT INTO Message(id, recipient_id, user_id, subject, body, date_sent) VALUES
+(1, 2, 1, 'Heylooo', 'Where art thou?',CURDATE());
+
+
+CREATE TABLE Message_read (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+message_id int  NOT NULL, 
+reader_id int NOT NULL,
+date date
+);
+
+INSERT INTO Message_read(id, message_id, reader_id, date) VALUES
+(1, 1, 1, CURDATE());pent
